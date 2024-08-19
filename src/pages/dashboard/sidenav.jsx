bccc452abe7766d1aecb5f3eb7e2ef12434319f0
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 23:09:49 +0300

    sidenav

--- src/pages/dashboard/sidenav.jsx
+++ src/pages/dashboard/sidenav.jsx
@@ -21,39 +21,39 @@ const SideNav=()=>{
     </div>
     <div className=" border-b border-slate-400 items-center xl:my-2 my-8 w-100 py-4"> 
     <Link to="/" className={`flex flex-col items-center justify-evenly${isSelected?"bg-stone-100":"bg-transparent"}`}> 
-<TfiDashboard size={'40'} color="white"/>
+<TfiDashboard size={'32'} color="white"/>
 <h1 className="text-xl text-stone-200">Dashboard</h1>
 </Link>
     </div>
     <div className=" border-b border-slate-400 items-center xl:my-2 my-8 w-100 py-4"> 
     <Link to="/students" className={`flex flex-col items-center justify-evenly${isSelected?"bg-stone-100":"bg-transparent"}`}> 
-<PiStudentBold size={'40'} color="white"/>
+<PiStudentBold size={'32'} color="white"/>
 <h1 className="text-xl text-stone-200">Students</h1>
 </Link>
 
     </div>
     {isAuthenticated &&(<div className=" border-b border-slate-400 items-center xl:my-2 my-8 w-100 py-4"> 
     <Link to="/teachers" className="flex flex-col items-center justify-evenly"> 
-<FaChalkboardTeacher size={'40'} color="white"/>
+<FaChalkboardTeacher size={'32'} color="white"/>
 <h1 className="text-xl text-stone-200">Teachers</h1>
 </Link>
     </div>)}
     
     <div className=" border-b border-slate-400 items-center xl:my-2 my-8 w-100 py-4"> 
     <Link to="/classes" className="flex flex-col items-center justify-evenly"> 
-<SiGoogleclassroom size={'40'} color="white"/>
+<SiGoogleclassroom size={'32'} color="white"/>
 <h1 className="text-xl text-stone-200">Classrooms</h1>
 </Link>
     </div>
     <div className=" border-b border-slate-400 items-center xl:my-2 my-8 w-100 py-4"> 
     <Link to="/timetable" className="flex flex-col items-center justify-evenly"> 
-<MdOutlineAccessTimeFilled size={'40'} color="white"/>
+<MdOutlineAccessTimeFilled size={'32'} color="white"/>
 <h1 className="text-xl text-stone-200">TimeTable</h1>
 </Link>
     </div>
     <div className=" border-b border-slate-400 items-cente xl:my-2 my-8 w-100 py-4"> 
     <Link to="/timetable" className="flex flex-col items-center justify-evenly"> 
-<MdAccountCircle size={'40'} color="white"/>
+<MdAccountCircle size={'32'} color="white"/>
 <h1 className="text-xl text-stone-200">Account</h1>
 </Link>
     </div>
