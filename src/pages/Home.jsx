6a7be9dac8cf598f04e768f9a95e7c3b4513e687
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 10:22:07 +0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Dashboard from "./dashboard/dashboard"
 import SideNav from "./dashboard/sidenav"
-
+import { selectCurrentUser } from "../features/auth/AuthSlice"
+import { useSelector } from "react-redux"
 const Home=()=>{
+    const user = useSelector(selectCurrentUser)
     return(
         <div className="flex">
            
             <SideNav/>
-            <h1>Hello Sudent</h1>
+          
            <Dashboard/>
            
         </div>
