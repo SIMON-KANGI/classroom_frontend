887eb73b68f4524c115279cf67449f6e5f8475f6
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 10:25:21 +0300

    select field

--- src/components/SelectField.jsx
+++ src/components/SelectField.jsx
@@ -1,9 +1,28 @@
+import React from 'react';
 
+const SelectField = ({ title, options, value, onChange }) => {
+  return (
+    <div className="select-field">
+      <label htmlFor={title} className="block text-gray-700 font-bold mb-2">
+        {title}
+      </label>
+      <select
+        id={title}
+        value={value}
+        onChange={e => onChange(e.target.value)}
+        className="p-2 border rounded"
+      >
+        <option value="" disabled>
+          Select {title}
+        </option>
+        {options.map((option, index) => (
+          <option key={index} value={option.label}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
-const SelectField=()=>{
-    return(
-        <select>
-
-        </select>
-    )
-}
+export default SelectField;
