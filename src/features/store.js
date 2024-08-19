559b097834f4d0bcb79222ca7a61d548a371271c
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 23:10:56 +0300

    redux store

--- src/features/store.js
+++ src/features/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from './auth/AuthSlice'
 import {apiSlice} from './auth/authApi'
-
+import userReducer from './userSlice'
+import classReducer from './classRoomSlice'
+import timetableReducer from './timetableSlice'
 const store= configureStore({
     reducer:{
         auth:AuthReducer,
+        user:userReducer,  
+        class:classReducer,
+        timetables:timetableReducer,
         api:apiSlice.reducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
