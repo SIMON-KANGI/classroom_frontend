02220b6dc035b46beb5f7aa15c2c8ddb93af9a04
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 10:21:21 +0300

    classrooms

--- src/pages/dashboard/Classroom.jsx
+++ src/pages/dashboard/Classroom.jsx
@@ -1,12 +1,19 @@
 import CreateClassroom from "../CreateClassroom"
 import SideNav from "./sidenav"
 
-
+import useFetch from "../../hooks/useFetch"
 const Classroom=()=>{
+const {data:classes}= useFetch('http://localhost:3000/api/classes/')
     return(
         <div className="flex">
 <SideNav/>
-<CreateClassroom/>
+<section>
+<h1 className="text-5xl my-8">Classrooms</h1>
+  <CreateClassroom/>  
+ {}
+ 
+</section>
+
         </div>
     )
 }
