f024afb1b2b5ab83f8544a63b19d6d996aea6909
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 10:25:54 +0300

    formfield

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
-function FormField({title,value,name, handleChange, placeholder}) {
+function FormField({title,value,name, handleChange, placeholder,select}) {
   return (
     <form>
     <div className='flex flex-col'>
-    <label>{title}</label>
+    <label className='font-bold'>{title}</label>
         <input name={name} value={value} onChange={handleChange} placeholder={placeholder} 
-          className="border rounded-lg"
+          className="border-2 py-2 px-4 rounded-lg"
         />
+        
     </div>
-      
+      <div>
+        {select}
+      </div>
     </form>
   )
 }
