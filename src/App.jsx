53245aebd7ab7010afb2509128c525c47f2741ac
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 23:11:59 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/login'
 import Home from './pages/Home'
-import Students from './pages/dashboard/Students'
-import Teachers from './pages/dashboard/Teachers'
-import Classroom from './pages/dashboard/Classroom'
+import Students from './pages/dashboard/students/Students'
+import Teachers from './pages/dashboard/teachers/Teachers'
+import Classroom from './pages/dashboard/classroom/Classroom'
+import { selectCurentToken } from './features/auth/AuthSlice'
+import { useSelector } from'react-redux'
+import TimeTable from './pages/dashboard/timetable/timetable'
 function App() {
+  const token = useSelector(selectCurentToken)
+
+  if (!token) {
+    return <Login />
+  }
   return (
-    <main className="flex h-fit overflow-hidden">
+    <main className=" main flex h-screen xl:overflow-auto overflow-hidden">
       <Routes>
         <Route path="/login" element={<Login/>}/>
         <Route path="/" element={<Home/>}/>
         <Route path="/students" element={<Students/>}/>
         <Route path="/teachers" element={<Teachers/>}/>
         <Route path="/classes" element={<Classroom/>}/>
+        <Route path="/timetable" element={<TimeTable/>}/>
       </Routes>
     </main>
   )
